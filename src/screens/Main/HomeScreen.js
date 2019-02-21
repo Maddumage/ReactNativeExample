217eabcbd511d26f0a52228f09da9a78e2bd0834
Roshan Maddumage <uomroshan@gmail.com>
2019-02-21 22:36:04 +0530

    EStyleSheet added

--- src/screens/Main/HomeScreen.js
+++ src/screens/Main/HomeScreen.js
@@ -1,16 +1,31 @@
 //import liraries
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import EStyleSheet from "react-native-extended-stylesheet";
 
 // create a component
 class HomeScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>Home Screen</Text>
+      <View style={styles.mainContainer}>
+        <Text style={styles.text}>Home screen</Text>
       </View>
     );
   }
 }
+
+const styles = EStyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "blue"
+  },
+  text: {
+    color: "white",
+    fontSize: "16rem",
+    fontWeight: "bold"
+  }
+});
 
 export default HomeScreen;
--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import EStyleSheet from "react-native-extended-stylesheet";
 
 class SplashScreen extends Component {
   state = {};
   render() {
     return (
-      <View>
-        <Text>Splash Screen</Text>
+      <View style={styles.mainContainer}>
+        <Text style={styles.text}>Splash screen</Text>
       </View>
     );
   }
 }
+
+const styles = EStyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "blue"
+  },
+  text: {
+    color: "white",
+    fontSize: "16rem",
+    fontWeight: "bold"
+  }
+});
 
 export default SplashScreen;
